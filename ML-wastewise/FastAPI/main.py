1dfe349c635c716dc948d73b91ac7a6dbe586d3f
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-29 17:03:04 +0700

    fix main.py

--- ML-wastewise/FastAPI/main.py
+++ ML-wastewise/FastAPI/main.py
@@ -82,3 +82,8 @@ async def classify_waste(request: Request, file: UploadFile = File(...)):
         os.remove(temp_file)
 
     return templates.TemplateResponse("index.html", {"request": request, "result": result})
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
